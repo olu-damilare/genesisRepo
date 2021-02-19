c401d7073675744a3dcc501ccd89269d475a4138
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-19 13:34:00 +0100

    Exercises and Practice

---
+++ Television.java
@@ -0,0 +1,106 @@
+public class Television {
+	private boolean isOn;
+	private String name;
+	private int volume;
+	private int channel;
+
+	public Television(String name){
+		this.name = name;
+	}
+
+	public void increaseVolume(){
+		if(isOn == true){
+			if (volume  >= 0 && volume < 100){
+				volume += 1;
+			}
+			else
+				System.out.println("The volume cannot increase beyond 100");
+		}
+		else
+		System.out.println("The television is switched off");
+	}
+
+	public void decreaseVolume(){
+		if(isOn == true){
+			if (volume <= 100 && volume >= 1){	
+				volume -= 1;
+			}
+			else
+				System.out.println("The volume cannot decrease below 0");
+			
+		}
+		else
+			System.out.println("The television is switched off");
+	}
+	
+	public void setVolume(int newVolume){
+		if(isOn == true){
+			if(newVolume >= 0 && newVolume <= 100)
+			volume = newVolume;
+			else
+			System.out.println("invalid volume number");
+		}
+		else
+		System.out.println("Television is switched off");
+	}
+	public int getVolume(){
+		return volume;
+	}
+	
+	public boolean isOn(){
+		return isOn;
+	}
+	public void setOn(boolean switchOn){
+		isOn = switchOn;
+		if (isOn == true){
+			channel = 1;
+			volume = 50;
+		}
+		else
+			System.out.println("Television is switched off");
+	} 
+	
+	public String getName(){
+		return name;
+	}
+	public void setChannel(int newChannel){
+		if(isOn == true){
+			if(newChannel >=1 && newChannel <=150)
+				channel = newChannel;
+			else 
+				System.out.println("Invalid channel number");
+		}
+		else
+			System.out.println("Television is switched off");	
+	}
+	public void nextChannel(){
+		if(isOn == true){
+			if (channel > 0 && channel != 150){
+				channel += 1;
+			}
+			if(channel == 150)
+				channel = 1;
+		}
+		else
+		System.out.println("The television is switched off");
+
+	}
+	
+	
+	public void previousChannel(){
+		if(isOn == true){
+			if (channel != 1 && channel <= 150){
+				channel -= 1;
+			}
+			if(channel == 1)
+				channel = 150;
+		}
+		else
+		System.out.println("The television is switched off");
+
+	}
+	public int getChannel(){
+		return channel;
+	}
+
+}
---
+++ TelevisionTest.java
@@ -0,0 +1,55 @@
+public class TelevisionTest{
+
+	public static void main (String... args){
+
+	Television myTelevision = new Television("LG");
+	myTelevision.setOn(true);
+	
+	System.out.println(myTelevision.isOn());
+	System.out.println("The volume is " + myTelevision.getVolume());
+	System.out.println("This television is an " + myTelevision.getName() + " product.");
+
+	myTelevision.setVolume(1);
+	myTelevision.decreaseVolume();
+	System.out.println("The volume is " + myTelevision.getVolume());
+	myTelevision.decreaseVolume();
+	System.out.println("The volume is " + myTelevision.getVolume());
+
+	myTelevision.setVolume(96);
+	System.out.println("The volume is " + myTelevision.getVolume());
+	
+	myTelevision.increaseVolume();
+	myTelevision.increaseVolume();
+	myTelevision.increaseVolume();
+	myTelevision.increaseVolume();
+	myTelevision.increaseVolume();
+	System.out.println("The volume is " + myTelevision.getVolume());
+	
+	myTelevision.decreaseVolume();
+	System.out.println("The volume is " + myTelevision.getVolume());
+	
+	myTelevision.setChannel(1);
+	System.out.println("Channel " + myTelevision.getChannel());
+
+	myTelevision.previousChannel();
+	System.out.println("channel " + myTelevision.getChannel());
+
+	myTelevision.previousChannel();
+	myTelevision.previousChannel();
+	myTelevision.previousChannel();
+
+	System.out.println("channel " + myTelevision.getChannel());
+
+
+	myTelevision.setChannel(150);
+	myTelevision.nextChannel();
+	System.out.println("channel " + myTelevision.getChannel());
+	
+	
+	
+	
+	}
+
+
+
+}
