60fdc45352290c56f176e417264c10219a5a45ba
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-23 21:40:02 +0100

    Exercise

---
+++ Chapter 4/ValidatingUserInput.java
@@ -0,0 +1,35 @@
+package chapterFour;
+
+import java.util.Scanner;
+
+public class ValidatingUserInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int result;
+        int passes = 0;
+        int failures = 0;
+        int studentCounter = 1;
+
+
+
+        while(studentCounter <= 10) {
+            System.out.println("Enter result (1 = pass, 2 = fail): ");
+            result = input.nextInt();
+
+            if(result == 1) {
+                passes += 1;
+                studentCounter++;
+            }
+            else if(result == 2) {
+                failures += 1;
+                studentCounter++;
+            }
+        }
+
+        System.out.printf("Passed: %d%nFailed: %d%n", passes, failures);
+
+        if(passes > 8)
+            System.out.println("Bonus to instructor");
+    }
+}
