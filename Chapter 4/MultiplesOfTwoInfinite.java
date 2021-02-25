4029fcb53dbe46790a58cf499ac255d0830a10be
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-25 19:46:49 +0100

    Exercise

---
+++ Chapter 4/MultiplesOfTwoInfinite.java
@@ -0,0 +1,14 @@
+package chapterFour;
+
+public class MultiplesOfTwoInfinite {
+    public static void main(String[] args) {
+        int startValue = 2;
+        while(startValue % 2 == 0){
+            System.out.println(startValue);
+            startValue *= 2;
+        }
+
+
+
+    }
+}
