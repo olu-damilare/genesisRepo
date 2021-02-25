b538f162934e4e01a08adb7057b7ab4ba2514a39
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-25 19:41:42 +0100

    Exercise

---
+++ Chapter 4/CryptographyTest.java
@@ -0,0 +1,9 @@
+package chapterFour;
+
+public class CryptographyTest {
+    public static void main(String[] args) {
+        Cryptography encrypt = new Cryptography();
+
+       System.out.println(encrypt.encode(2317));
+    }
+}
