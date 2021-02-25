9910b04e6e6f050687848a847d90707c249ba962
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-25 19:40:33 +0100

    Exercise

---
+++ Chapter 4/AsteriskCheckerBoardTest.java
@@ -0,0 +1,9 @@
+package chapterFour;
+
+public class AsteriskCheckerBoardTest {
+    public static void main(String[] args) {
+        AsteriskCheckerBoard checkerBoard = new AsteriskCheckerBoard();
+
+        checkerBoard.buildCheckerBoard(13);
+    }
+}
