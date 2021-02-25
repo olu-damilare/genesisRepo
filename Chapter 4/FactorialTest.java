19a44ac741c02b4e4837288874f100c77312361b
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-25 19:43:00 +0100

    Exercise

---
+++ Chapter 4/FactorialTest.java
@@ -0,0 +1,10 @@
+package chapterFour;
+
+public class FactorialTest {
+    public static void main(String[] args) {
+        Factorial ojoro = new Factorial();
+
+        int factorial = ojoro.calculateFactorial(10);
+        System.out.println(factorial);
+    }
+}
