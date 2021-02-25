1b9146c9619e143fa057168c13f8d427ae2d7981
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-25 19:46:09 +0100

    Exercise

---
+++ Chapter 4/FiveIntegarPalindromeTest.java
@@ -0,0 +1,9 @@
+package chapterFour;
+
+public class FiveIntegarPalindromeTest {
+    public static void main(String[] args) {
+        FiveIntegarPalindrome palindrome = new FiveIntegarPalindrome();
+
+        palindrome.isPalindrome(54445);
+    }
+}
