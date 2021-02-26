1d874c875ee9acd98ff6805703b6e776e6862cd2
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-26 20:12:42 +0100

    Exercise

---
+++ Chapter 4/BinaryToDecimalTest.java
@@ -0,0 +1,11 @@
+package chapterFour;
+
+public class BinaryToDecimalTest {
+    public static void main(String[] args) {
+
+        BinaryToDecimal binary = new BinaryToDecimal();
+        int decimal = binary.convertBinaryToDecimal(10);
+        System.out.println(decimal);
+
+    }
+}
