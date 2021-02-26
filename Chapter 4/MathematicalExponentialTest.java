2e383a3027319d2aeb87d7047a7cb9751908fa1a
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-26 20:13:56 +0100

    Exercise

---
+++ Chapter 4/MathematicalExponentialTest.java
@@ -0,0 +1,10 @@
+package chapterFour;
+
+public class MathematicalExponentialTest {
+    public static void main(String[] args) {
+        MathematicalExponential calculate = new MathematicalExponential();
+
+        System.out.printf("%.2f", calculate.calculateExponential(5));
+
+    }
+}
