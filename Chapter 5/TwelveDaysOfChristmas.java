a99246056b1d158fd3bcd8fb007e0dcd966e4836
Damilare Jolayemi <djolayemi@gmail.com>
2021-02-27 22:45:13 +0100

    Exercises

--- Chapter 5/TwelveDaysOfChristmas.java
+++ Chapter 5/TwelveDaysOfChristmas.java
@@ -52,28 +52,17 @@ public class TwelveDaysOfChristmas {
             }
 
             switch (dayCounter) {
-                case 12:
-                    System.out.println("Twelve drummers drumming,");
-                case 11:
-                    System.out.println("Eleven pipers piping");
-                case 10:
-                    System.out.println("Ten lords a-leaping");
-                case 9:
-                    System.out.println("Nine ladies dancing");
-                case 8:
-                    System.out.println("Eight maids a-milking");
-                case 7:
-                    System.out.println("Seven swans a-swimming");
-                case 6:
-                    System.out.println("Six geese a-laying");
-                case 5:
-                    System.out.println("Five golden rings");
-                case 4:
-                    System.out.println("Four calling birds");
-                case 3:
-                    System.out.println("Three French hens");
-                case 2:
-                    System.out.println("Two turtle doves");
+                case 12: System.out.println("Twelve drummers drumming,");
+                case 11: System.out.println("Eleven pipers piping");
+                case 10: System.out.println("Ten lords a-leaping");
+                case 9: System.out.println("Nine ladies dancing");
+                case 8: System.out.println("Eight maids a-milking");
+                case 7: System.out.println("Seven swans a-swimming");
+                case 6: System.out.println("Six geese a-laying");
+                case 5: System.out.println("Five golden rings");
+                case 4: System.out.println("Four calling birds");
+                case 3: System.out.println("Three French hens");
+                case 2: System.out.println("Two turtle doves");
                 case 1:
                     if (dayCounter == 1)
                         System.out.println("A partridge in a pear tree");
@@ -84,95 +73,6 @@ public class TwelveDaysOfChristmas {
             dayCounter++;
             System.out.println();
         }
-//    }
-//        public static void main(String[] args) {
-//
-//            int dayCounter = 1;
-//
-//            System.out.println("12 Days Of Christmas Song");
-//            while(dayCounter <= 12) {
-//                System.out.printf("Verse %d\n", dayCounter);
-//                System.out.print("On the ");
-//                printDay(dayCounter);
-//                System.out.print(" day of Christmas my true love sent to me");
-//                printRemainingVerses(dayCounter);
-//                ++dayCounter;
-//            }
-//        }
-//
-//        public static void printDay(int dayCounter){
-//            switch (dayCounter){
-//                case 1:
-//                    System.out.print("first");
-//                    break;
-//                case 2:
-//                    System.out.println("second");
-//                    break;
-//                case 3:
-//                    System.out.println("third");
-//                    break;
-//                case 4:
-//                    System.out.println("fourth");
-//                    break;
-//                case 5:
-//                    System.out.println("fifth");
-//                    break;
-//                case 6:
-//                    System.out.println("sixth");
-//                    break;
-//                case 7:
-//                    System.out.println("seventh");
-//                    break;
-//                case 8:
-//                    System.out.println("eighth");
-//                    break;
-//                case 9:
-//                    System.out.println("ninth");
-//                    break;
-//                case 10:
-//                    System.out.println("tenth");
-//                    break;
-//                case 11:
-//                    System.out.println("eleventh");
-//                    break;
-//                case 12:
-//                    System.out.println("twelfth");
-//            }
-//        }
-//
-//        public static void printRemainingVerses(int dayCounter){
-//            switch (dayCounter){
-//                case 12:
-//                    System.out.println("Twelve drummers drumming");
-//                case 11:
-//                    System.out.println("Eleven pipers piping");
-//                case 10:
-//                    System.out.println("Ten lords a-leaping");
-//                case 9:
-//                    System.out.println("Nine ladies dancing");
-//                case 8:
-//                    System.out.println("Eight maids a-milking");
-//                case 7:
-//                    System.out.println("Seven swans a-swimming");
-//                case 6:
-//                    System.out.println("Six geese a-laying");
-//                case 5:
-//                    System.out.println("Five golden rings");
-//                case 4:
-//                    System.out.println("Four calling birds");
-//                case 3:
-//                    System.out.println("Three french hens");
-//                case 2 :
-//                    System.out.println("Two turtle doves");
-//                case 1:
-//                    if (dayCounter == 1){
-//                        System.out.println("A partridge in a pear tree ");
-//                    }
-//                    else {
-//                        System.out.println("And a partridge in a pear tree");
-//                    }
-//                    break;
-//            }
-//        }
+
     }
 }
